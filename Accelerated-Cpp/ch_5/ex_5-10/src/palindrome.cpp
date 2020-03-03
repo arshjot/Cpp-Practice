47e6ce5e96dd71dc2e1db640485df8485c714153
arshjot <arshjot@live.com>
2020-03-03 17:02:21 +0530

    Rectify longest_string function

--- Accelerated-Cpp/ch_5/ex_5-10/src/palindrome.cpp
+++ Accelerated-Cpp/ch_5/ex_5-10/src/palindrome.cpp
@@ -36,16 +36,14 @@ bool is_palindrome(const string& v)
 
 string longest_string(const vector<string>&v)
 {
-	string::size_type max_len = 0;
-	string longest_s;
+    string longest_s;
 
     // get the string with the maximum length
     for (vector<string>::const_iterator iter = v.begin();
         iter != v.end(); ++iter) {
         
-        if (max_len < iter->length())
-        	max_len = max(max_len, iter->length());
-        	longest_s = *iter;
+        if (iter->length() > longest_s.length())
+            longest_s = *iter;
     }
 
     return longest_s;
